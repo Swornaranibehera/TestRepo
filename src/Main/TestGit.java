38080b11204f571ac71a279caef032b6d42a3805
Swornaranibehera <swornarani68@gmail.com>
2026-02-12 23:37:08 +0530

    second commit

--- src/Main/TestGit.java
+++ src/Main/TestGit.java
@@ -1,10 +1,11 @@
-package Main;
+
 
 public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Ashutosh");
+			System.out.println("Java 1st commit ");
+			System.out.println("Java 2nd commit ");
 			
 	}
 }
